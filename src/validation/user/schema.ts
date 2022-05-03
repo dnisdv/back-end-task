a7ec0985c4bbc9edccd28d932cbf8e4234eb8456
dnisdv <iamdenisgradinaru@gmail.com>
2022-05-03 19:17:32 +0300

    dont saved the file before commit...

--- src/validation/user/schema.ts
+++ src/validation/user/schema.ts
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 const EMAIL_VALIDATION_REGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-export const UserSchema = Joi.object({
+export const LoginUserSchema = Joi.object({
     name: Joi.string()
         .min(3)
         .max(30)
@@ -16,4 +16,21 @@ export const UserSchema = Joi.object({
         .min(3)
         .required(),
 });
+
+
+export const RegisterUserSchema = Joi.object({
+    name: Joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+
+    email: Joi.string()
+        .regex(EMAIL_VALIDATION_REGEX)
+        .required(),
+
+    password: Joi.string()
+        .min(3)
+        .required(),
+});
+
 
